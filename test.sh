d7ca5b3bd4ffe0d2a14b89d3781e91a6c95c5afe
Aleksei Kozlov <aleksei.e.kozlov@gmail.com>
2019-04-20 01:49:07 +0300

    test update

--- test.sh
+++ test.sh
@@ -4,4 +4,9 @@ cd build
 TEST_DIR=../data/1
 INPUT_TYPE=text
 ./stream_search $TEST_DIR/dict.txt $INPUT_TYPE < $TEST_DIR/input.txt > $TEST_DIR/output.txt
-diff $TEST_DIR/output.txt $TEST_DIR/output_etalon.txt
+diff $TEST_DIR/output.txt $TEST_DIR/output_etalon_$INPUT_TYPE.txt
+
+TEST_DIR=../data/1
+INPUT_TYPE=html
+./stream_search $TEST_DIR/dict.txt $INPUT_TYPE < $TEST_DIR/input.txt > $TEST_DIR/output.txt
+diff $TEST_DIR/output.txt $TEST_DIR/output_etalon_$INPUT_TYPE.txt
