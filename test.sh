9ef3f24c6882ec587eabcf86bd1514d22b02cb5b
Aleksei Kozlov <aleksei.e.kozlov@gmail.com>
2019-04-20 01:57:44 +0300

    test update

--- test.sh
+++ test.sh
@@ -1,12 +1,21 @@
 #!/bin/bash
-cd build
 
-TEST_DIR=../data/1
-INPUT_TYPE=text
-./stream_search $TEST_DIR/dict.txt $INPUT_TYPE < $TEST_DIR/input.txt > $TEST_DIR/output.txt
-diff $TEST_DIR/output.txt $TEST_DIR/output_etalon_$INPUT_TYPE.txt
+function test {
+    local TEST_DIR=$1
+    local INPUT_TYPE=$2
+    
+    ./stream_search $TEST_DIR/dict.txt $INPUT_TYPE < $TEST_DIR/input.txt > $TEST_DIR/output.txt
+    
+    diff $TEST_DIR/output.txt $TEST_DIR/output_etalon_$INPUT_TYPE.txt
+    RETURN_CODE=$?; 
+    
+    if [[ $RETURN_CODE != 0 ]]; then 
+    echo not matches etalon at $TEST_DIR input_type=$INPUT_TYPE
+    exit $RETURN_CODE; 
+    fi
+}
 
-TEST_DIR=../data/1
-INPUT_TYPE=html
-./stream_search $TEST_DIR/dict.txt $INPUT_TYPE < $TEST_DIR/input.txt > $TEST_DIR/output.txt
-diff $TEST_DIR/output.txt $TEST_DIR/output_etalon_$INPUT_TYPE.txt
+cd ./build
+
+test ../data/1 text
+test ../data/1 html
