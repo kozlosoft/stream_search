e8af6074fb19f2ba4040524f31eefc4189187afc
Aleksei Kozlov <aleksei.e.kozlov@gmail.com>
2019-04-22 03:59:50 +0300

    frontend update

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,29 +6,38 @@ class App extends Component {
   defaultHtml = '<div><i class="src">abc</i>abcdef <i class="src">abc</i> <i class="src">aa</i></div>';
   state = {
     html: this.defaultHtml,
-    wordToHighlight: "not selected"
+    wordToHighlight: ""
+  }
+
+  componentDidMount() {
+    const uniqueWords = this.getUniqueWords();
+    if (this.state.wordToHighlight === "" && uniqueWords.length > 0) {
+      this.handleWordChange({
+        target: {
+          value: uniqueWords[0]
+        }
+      });
+    }
   }
 
   render() {
-    const elements = $('i.src', this.state.html);
-    const uniqueWords = [...new Set(
-      elements.get().map(item => { return item.innerText}))];
-    const options = uniqueWords.map(word => 
-      <option key={word} value={word}>{word}</option> 
+    const uniqueWords = this.getUniqueWords();
+    const options = uniqueWords.map(word =>
+      <option key={word} value={word}>{word}</option>
     );
 
     return (
       <div className="App">
         <p>insert html:</p>
-        <input 
+        <input
           defaultValue={this.defaultHtml}
           onChange={this.handleHtmlChange}></input>
-        <p>select a word to highlight: </p>
+        <p>select a word to <span className="highlight">highlight</span>: </p>
         <select value={this.state.wordToHighlight} onChange={this.handleWordChange}>
-            {options}
-          </select>
+          {options}
+        </select>
         <p>rendered html:</p>
-        <div dangerouslySetInnerHTML={{__html: this.state.html}}></div>
+        <div dangerouslySetInnerHTML={{ __html: this.state.html }}></div>
       </div>
     );
   }
@@ -40,9 +49,23 @@ class App extends Component {
   }
 
   handleWordChange = (e) => {
+    let guestHtml = $(this.state.html);
+
+    let elements = $(`i.src`, guestHtml);
+    elements.removeClass("highlight");
+
+    elements = $(`i.src:contains(${e.target.value})`, guestHtml);
+    elements.addClass("highlight");
     this.setState({
-      wordToHighlight: e.target.value
+      wordToHighlight: e.target.value,
+      html: guestHtml.prop("outerHTML")
     })
+  }
+
+  getUniqueWords = () => {
+    const elements = $('i.src', this.state.html);
+    return [...new Set(
+      elements.get().map(item => { return item.innerText }))];
   }
 }
 
